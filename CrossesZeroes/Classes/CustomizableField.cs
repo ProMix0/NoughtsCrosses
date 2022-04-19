44f136f9e3a118a1905a504b0fc8e34478d63f65
ProMix0 <misha.zavertkin@mail.ru>
2022-04-19 22:29:01 +0700

    Complete reaslization

--- CrossesZeroes/Classes/CustomizableField.cs
+++ CrossesZeroes/Classes/CustomizableField.cs
@@ -11,8 +11,8 @@ namespace CrossesZeroes.Classes
     {
 
         public CustomizableField(IOptions<Configuration> config)
-            :this(config.Value.Height,config.Value.Width,config.Value.WinSequenceLength)
-        {}
+            : this(config.Value.Height, config.Value.Width, config.Value.WinSequenceLength)
+        { }
 
         public CustomizableField(int height, int width, int winSequenceLength)
         {
@@ -24,7 +24,7 @@ namespace CrossesZeroes.Classes
             readonlyField = new(this);
         }
 
-        public CellState this[int i, int j] => field[i,j];
+        public CellState this[int i, int j] => field[i, j];
         private CellState[,] field;
 
         public int Width { get; }
@@ -33,21 +33,112 @@ namespace CrossesZeroes.Classes
 
         protected int winSequenceLength;
 
-        private ReadonlyField readonlyField;
+        private readonly ReadonlyField readonlyField;
         public ICrossesZeroesField AsReadonly() => readonlyField;
 
-        public void Clear()
+        public virtual void Clear()
         {
             field = new CellState[Height, Width];
         }
 
-        //TODO
-        public bool IsEndGame(out CellState winner)
+        public virtual bool IsEndGame(out CellState winner)
         {
-            throw new NotImplementedException();
+            //Перебор всех победных последовательностей
+            foreach (var seq in WinIndexes())
+            {
+                //Если все крестики
+                bool crosses = true;
+                foreach (var point in seq)
+                {
+                    if (field[point.x, point.y] != CellState.Cross)
+                    {
+                        crosses = false;
+                        break;
+                    }
+                }
+                if (crosses)
+                {
+                    //...то крестики победили
+                    winner = CellState.Cross;
+                    return true;
+                }
+
+                //Если все нолики
+                bool zeroes = true;
+                foreach (var point in seq)
+                {
+                    if (field[point.x, point.y] != CellState.Zero)
+                    {
+                        zeroes = false;
+                        break;
+                    }
+                }
+                if (zeroes)
+                {
+                    //...то нолики победили
+                    winner = CellState.Zero;
+                    return true;
+                }
+            }
+
+
+            winner = CellState.Empty;
+            for (int i = 0; i < field.GetLength(0); i++)
+                for (int j = 0; j < field.GetLength(1); j++)
+                    //Если хоть одна клетка пустая, то игра не завершена
+                    if (field[i, j] == CellState.Empty) return false;
+            //Иначе ничья
+            return true;
         }
 
-        public void Set(Point point, CellState markType)
+        protected virtual IEnumerable<IEnumerable<Point>> WinIndexes()
+        {
+            //Вертикальные
+            for (int i = 0; i <= field.GetLength(0) - winSequenceLength; i++)
+                for (int j = 0; j < field.GetLength(1); j++)
+                    yield return GetCol(i, j);
+
+            IEnumerable<Point> GetCol(int i, int j)
+            {
+                for (int k = 0; k < winSequenceLength; k++)
+                    yield return new(i + k, j);
+            }
+
+            //Горизонтальные
+            for (int i = 0; i < field.GetLength(0); i++)
+                for (int j = 0; j <= field.GetLength(1) - winSequenceLength; j++)
+                    yield return GetRow(i, j);
+
+            IEnumerable<Point> GetRow(int i, int j)
+            {
+                for (int k = 0; k < winSequenceLength; k++)
+                    yield return new(i, j + k);
+            }
+
+            //Диагональные возрастающие
+            for (int i = winSequenceLength - 1; i < field.GetLength(0); i++)
+                for (int j = 0; j <= field.GetLength(1) - winSequenceLength; j++)
+                    yield return GetUpDiagonal(i, j);
+
+            IEnumerable<Point> GetUpDiagonal(int i, int j)
+            {
+                for (int k = 0; k < winSequenceLength; k++)
+                    yield return new(i - k, j + k);
+            }
+
+            //Диагональные убывающие
+            for (int i = 0; i <= field.GetLength(0) - winSequenceLength; i++)
+                for (int j = 0; j <= field.GetLength(1) - winSequenceLength; j++)
+                    yield return GetDownDiagonal(i, j);
+
+            IEnumerable<Point> GetDownDiagonal(int i, int j)
+            {
+                for (int k = 0; k < winSequenceLength; k++)
+                    yield return new(i + k, j + k);
+            }
+        }
+
+        public virtual void Set(Point point, CellState markType)
         {
             if (markType == CellState.Empty) throw new ArgumentException("", nameof(markType));
             if (field[point.x, point.y] != CellState.Empty) throw new ArgumentException("", nameof(point));
@@ -57,9 +148,20 @@ namespace CrossesZeroes.Classes
 
         public class Configuration
         {
+            public static readonly string SectionName = nameof(CustomizableField);
+
             public int Width { get; set; }
             public int Height { get; set; }
             public int WinSequenceLength { get; set; }
+
+            public static bool Validate(Configuration configuration)
+            {
+                return configuration.Width > 0
+                    && configuration.Height > 0
+                    && configuration.WinSequenceLength > 1
+                    && configuration.WinSequenceLength <= configuration.Width
+                    && configuration.WinSequenceLength <= configuration.Height;
+            }
         }
     }
 }
