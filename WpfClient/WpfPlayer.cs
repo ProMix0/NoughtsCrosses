d2cb0aa7e7fe1d2b610611d2984df583ac4037bc
ProMix <54300969+ProMix0@users.noreply.github.com>
2022-05-07 11:22:36 +0700

    Wpf thread call need fix

--- WpfClient/WpfPlayer.cs
+++ WpfClient/WpfPlayer.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks;
 using System.Windows.Controls;
 using CrossesZeroes.Abstractions;
 using CrossesZeroes.Common;
@@ -9,7 +10,9 @@ namespace WpfClient
     {
         private readonly WpfClient client;
 
-        protected int width=0, height=0;
+        protected int width = 0, height = 0;
+        protected Button[,]? btnMatr = null;
+        protected TaskCompletionSource<Button>? btnCompletionSource = null;
 
         public WpfPlayer(WpfClient client)
         {
@@ -36,21 +39,60 @@ namespace WpfClient
         public Point Turn(ICrossesZeroesField field)
         {
             EnsureGridSize(field.Height, field.Width);
+
+            for (int i = 0; i < field.Height; i++)
+                for (int j = 0; j < field.Width; j++)
+                    btnMatr![i, j].Content = field[i, j] switch
+                    {
+                        CellState.Cross => 'X',
+                        CellState.Zero => '0',
+                        _ => ' '
+                    };
+
+            btnCompletionSource = new();
+            Button button = btnCompletionSource.Task.Result;
+
+            for (int i = 0; i < field.Height; i++)
+                for (int j = 0; j < field.Width; j++)
+                    if (btnMatr![i, j] == button)
+                        return new(i, j);
+
+            throw new Exception("Button outside field");
         }
 
-        protected void EnsureGridSize(int height,int width)
+        protected void EnsureGridSize(int height, int width)
         {
             if (height == this.height && width == this.width) return;
 
             Grid grid = new();
             client.field = grid;
+            btnMatr = new Button[height, width];
 
             for (int i = 0; i < height; i++)
                 grid.RowDefinitions.Add(new());
             for (int i = 0; i < width; i++)
                 grid.ColumnDefinitions.Add(new());
 
+            for (int i = 0; i < height; i++)
+                for (int j = 0; j < width; j++)
+                {
+                    Button button = new();
 
+                    Grid.SetRow(button, i);
+                    Grid.SetColumn(button, j);
+
+                    button.Click += ButtonClick;
+
+                    btnMatr[i, j] = button;
+                    grid.Children.Add(button);
+                }
+        }
+
+        protected void ButtonClick(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (btnCompletionSource == null) return;
+
+            btnCompletionSource.SetResult((sender as Button)!);
         }
     }
 }
