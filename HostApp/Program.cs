79e1f270c6550b762d0a546ee170e042b790080b
ProMix0 <misha.zavertkin@mail.ru>
2022-05-10 21:19:30 +0700

    New running project

---
+++ HostApp/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+using CrossesZeroes.Abstractions;
+using CrossesZeroes.Classes;
+using CrossesZeroes.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using WpfClient;
+
+await Host.CreateDefaultBuilder()
+    .ConfigureAppConfiguration(config =>
+        config.AddJsonFile("Settings.json"))
+
+    //Добавление сервисов (классов)
+    .ConfigureServices((context, services) =>
+
+        //Позволят получить объект с настройками через DI
+        services
+        .AddOptions<CustomizableField.Configuration>()
+            .BindConfiguration(CustomizableField.Configuration.SectionName)
+            .Validate(CustomizableField.Configuration.Validate)
+            .Services
+
+        //AddTransient добавляет в коллекцию сервисов класс
+        //Первое обобщение говорит о запрашиваемом классе, второе - о возвращаемом
+        .AddTransient<CrossesZeroesAbstract, CrossesZeroesWithAi>()
+        .AddTransient<IRealPlayer, WpfPlayer>()
+        .AddTransient<IAiPlayer, AiPlayer>()
+        .AddTransient<ICrossesZeroesField, ExtraCustomizableField>()
+
+        //TODO inject by single metod
+        //.AddTransient<IWpfView, WpfView>()
+
+        .AddHostedService<CrossesZeroesLoopService>())
+
+    .ConfigureLogging(logging =>
+        logging.ClearProviders())
+
+    .RunConsoleAsync();
