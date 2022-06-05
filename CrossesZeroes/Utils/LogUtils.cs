a4f592948443d4c293018cebb41aa7e07d8ae696
ProMix0 <misha.zavertkin@mail.ru>
2022-06-06 01:16:16 +0700

    Move logging utils to Utils namespace

---
+++ CrossesZeroes/Utils/LogUtils.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace CrossesZeroes.Utils
+{
+    public static class LogUtils
+    {
+        public static void LogMessageAndThrow(this ILogger logger, Exception exception, LogLevel logLevel = LogLevel.Critical)
+        {
+            logger.Log(logLevel, exception,"Exception was thrown: {Message}", exception.Message);
+            throw exception;
+        }
+    }
+}
