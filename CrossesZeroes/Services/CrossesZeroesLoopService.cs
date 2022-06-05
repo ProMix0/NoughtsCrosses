c040888343c2b78bcb5f0f6983203f82f62f1118
ProMix0 <misha.zavertkin@mail.ru>
2022-06-06 01:18:14 +0700

    Using CriticalBackgroundService
    To prevent uncatched exceptions

--- CrossesZeroes/Services/CrossesZeroesLoopService.cs
+++ CrossesZeroes/Services/CrossesZeroesLoopService.cs
@@ -1,4 +1,5 @@
 ﻿using CrossesZeroes.Abstractions;
+﻿using BetterHostedServices;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
@@ -9,44 +10,38 @@ using System.Threading.Tasks;
 
 namespace CrossesZeroes.Services
 {
-    public class CrossesZeroesLoopService : BackgroundService
+    public class CrossesZeroesLoopService : CriticalBackgroundService
     {
         private readonly CrossesZeroesAbstract game;
         private readonly IHostLifetime lifetime;
 
-        public CrossesZeroesLoopService(CrossesZeroesAbstract game, IHostLifetime lifetime)
+        public CrossesZeroesLoopService(CrossesZeroesAbstract game, IHostApplicationLifetime lifetime, ILogger<CrossesZeroesLoopService> logger)
+            : base(null)
         {
             this.game = game;
             this.lifetime = lifetime;
+            this.logger = logger;
+        }
+
+        protected override void OnError(Exception exceptionFromExecuteAsync)
+        {
+            logger.LogMessageAndThrow(exceptionFromExecuteAsync);
         }
 
         protected async override Task ExecuteAsync(CancellationToken token)
         {
-            try
+            while (!token.IsCancellationRequested)
             {
-                while (!token.IsCancellationRequested)
-                {
-                    while (!token.IsCancellationRequested && await game.Turn())
-                        token.ThrowIfCancellationRequested();
+                while (!token.IsCancellationRequested && await game.Turn())
+                    token.ThrowIfCancellationRequested();
 
-                    bool restart = await game.IsRestartWanted();
-                    if (!restart) break;
-
-                    game.Restart();
-                }
+                bool restart = await game.IsRestartWanted();
+                if (!restart) break;
 
                 await lifetime.StopAsync(token);
+                game.Restart();
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.GetType());
-                Console.WriteLine();
 
-                Console.WriteLine(ex.Message);
-                Console.WriteLine();
-
-                Console.WriteLine(ex.StackTrace);
-            }
         }
     }
 }
