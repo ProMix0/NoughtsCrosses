d87f9ade09228e69cf788b2822a3ef6b6db21d2d
ProMix0 <misha.zavertkin@mail.ru>
2022-06-10 19:52:47 +0700

    Using AutoMoq

--- CrossesZeroes.Tests/AiPlayerTests.cs
+++ CrossesZeroes.Tests/AiPlayerTests.cs
@@ -1,3 +1,4 @@
+using AutoFixture.Xunit2;
 using CrossesZeroes.Abstractions;
 using CrossesZeroes.Classes;
 using CrossesZeroes.Common;
@@ -14,41 +15,45 @@ namespace CrossesZeroes.Tests
 {
     public class AiPlayerTests
     {
-        private static readonly TestField field = new ();
+        private static readonly TestField field = new();
 
-        private void FillField()
+        private void FillField(TestField field)
         {
             for (int i = 0; i < field.Height; i++)
                 for (int j = 0; j < field.Width; j++)
                     field[i, j] = CellState.Cross;
         }
 
-        public static IEnumerable<object[]> IterateIndexes()
+        public static IEnumerable<int[]> IterateIndexes(ICrossesZeroesField field)
         {
             for (int i = 0; i < field.Height; i++)
                 for (int j = 0; j < field.Width; j++)
-                    yield return new object[] { i, j };
+                    yield return new int[] { i, j };
         }
 
-        [Theory]
-        [MemberData(nameof(IterateIndexes))]
-        public void Turn_ShouldFillEmpty(int i,int j)
+        [Theory, AutoMoqData]
+        public void Turn_ShouldFillEmpty([Frozen] TestField field, AiPlayer player)
         {
-            FillField();
-            field[i, j] = CellState.Empty;
+            foreach (var arr in IterateIndexes(field))
+            {
+                int i = arr[0], j = arr[1];
 
-            AiPlayer player = new(Options.Create<AiPlayer.AiPlayerBehaviour>(new()), NullLogger<AiPlayer>.Instance);
-            player.Init(CellState.Cross, field);
+                FillField(field);
+                field[i, j] = CellState.Empty;
 
-            player.Turn().Result.Should().Be(new Point(i, j));
+                //AiPlayer player = new(Options.Create<AiPlayer.AiPlayerBehaviour>(new()), NullLogger<AiPlayer>.Instance);
+                player.Init(CellState.Cross, field);
+
+                player.Turn().Result.Should().Be(new Point(i, j));
+            }
         }
 
-        [Fact]
-        public void Turn_ShouldThrowFilled()
+        [Theory, AutoMoqData]
+        public void Turn_ShouldThrowFilled(TestField field, AiPlayer player)
         {
-            FillField();
+            FillField(field);
 
-            AiPlayer player = new(Options.Create<AiPlayer.AiPlayerBehaviour>(new()), NullLogger<AiPlayer>.Instance);
+            //AiPlayer player = new(Options.Create<AiPlayer.AiPlayerBehaviour>(new()), NullLogger<AiPlayer>.Instance);
             player.Init(CellState.Cross, field);
 
             player.Invoking(pl => pl.Turn()).Should().ThrowAsync<InvalidOperationException>();
@@ -59,7 +64,7 @@ namespace CrossesZeroes.Tests
         [InlineData(false)]
         public void RepeatWanted_ShouldReturnAsParam(bool behavior)
         {
-            AiPlayer player = new(Options.Create<AiPlayer.AiPlayerBehaviour>(new() { wantRepeat=behavior}), NullLogger<AiPlayer>.Instance);
+            AiPlayer player = new(Options.Create<AiPlayer.AiPlayerBehaviour>(new() { wantRepeat = behavior }), NullLogger<AiPlayer>.Instance);
             player.IsRepeatWanted().Should().Be(Task.FromResult(behavior));
         }
     }
--- CrossesZeroes.Tests/TestClasses/TestField.cs
+++ CrossesZeroes.Tests/TestClasses/TestField.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace CrossesZeroes.Tests.TestClasses
 {
-    internal class TestField : CrossesZeroesField
+    public class TestField : CrossesZeroesField
     {
         public TestField() : base(NullLogger<CrossesZeroesField>.Instance)
         {
