8042f037bb5c75076059fc7077093a4a8bd76a22
ProMix0 <misha.zavertkin@mail.ru>
2022-06-23 16:32:03 +0700

    Create DIUtils.cs
    Allows to add service both as Type and IImplementation

---
+++ NoughtsCrosses/Utils/DIUtils.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoughtsCrosses.Utils
+{
+    public static class DIUtils
+    {
+        public static IServiceCollection AddTypeAndImplementation<TType, TImplementation>(this IServiceCollection services)
+            where TImplementation : class, TType
+            where TType : class
+            =>
+            services.AddTransient<TType, TImplementation>().AddTransient<TImplementation>();
+    }
+}
